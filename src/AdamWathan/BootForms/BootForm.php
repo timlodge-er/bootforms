1c76c0dba528d2d4d5944592a4ad5e9e48a15d96
Tim Lodge <tim@edwardrobertson.co.uk>
2024-06-17 14:20:02 +0100

    Added further error reporting

--- src/AdamWathan/BootForms/BootForm.php
+++ src/AdamWathan/BootForms/BootForm.php
@@ -27,6 +27,14 @@ class BootForm
 
     public function __call($method, $parameters)
     {
+        if (is_null($this->builder)) {
+            throw new \BadMethodCallException("Builder is not initialized. Call open() or openHorizontal() first.");
+        }
+
+        if (!method_exists($this->builder, $method)) {
+            throw new \BadMethodCallException("Method {$method} does not exist on the builder object.");
+        }
+
         return call_user_func_array([$this->builder, $method], $parameters);
     }
 }
